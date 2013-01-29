0d8a8ac15be2d785b6151ef60783512767ff2b3b
paramvir-b <paramvir@rokoder.com>
2013-01-28 18:47:20 -0600

    Base line code to use for performance enhancement.
    This code does not have any sqlite call. So this has to perform as fast as possible

--- cpp/src/main.cpp
+++ cpp/src/main.cpp
@@ -10,7 +10,7 @@
 #include <string.h>
 #include <stdlib.h>
 
-#include "sqlite3.h"
+//#include "sqlite3.h"
 #include "cJSON.h"
 #include "OptionParser.h"
 
@@ -58,6 +58,7 @@ string & replaceAll(std::string& str, const std::string& from, const std::string
 }
 
 int main(int argc, char **argv) {
+    clock_t cStartClock;
 
     OptionParser parser = OptionParser() .description("Converts fixed length files to sqlite database");
 
@@ -82,6 +83,7 @@ int main(int argc, char **argv) {
     long commitAfter = atol(options["c"].c_str());
     bool isDebug = atoi(options["d"].c_str()) == 1? true : false;
 
+    cStartClock = clock();
     if(isDebug) {
         cout<<"layoutFileName="<<layoutFileName<<endl;
         cout<<"inputFileName="<<inputFileName<<endl;
@@ -155,54 +157,55 @@ int main(int argc, char **argv) {
     remove(dbFileName.c_str());
 
     int rc;
-    sqlite3 *db;
+    // sqlite3 *db;
     char *zErrMsg = 0;
 
-    rc = sqlite3_open(dbFileName.c_str(), &db);
-    if(rc) {
-        fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
-        sqlite3_close(db);
-        return(1);
-    }
-    //sqlconn = sqlite3.connect(dbFileName);
-    //cursor = sqlconn.cursor();
-    rc = sqlite3_exec(db, createTableQry.c_str(), NULL, 0, &zErrMsg);
-    if(rc != SQLITE_OK) {
-        fprintf(stderr, "SQL error: %s\n", zErrMsg);
-        sqlite3_free(zErrMsg);
-        return 1;
-    }
+    /*
+       rc = sqlite3_open(dbFileName.c_str(), &db);
+       if(rc) {
+       fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
+       sqlite3_close(db);
+       return(1);
+       }
 
-    rc = sqlite3_exec(db, "PRAGMA synchronous=OFF;", NULL, 0, &zErrMsg);
-    if(rc != SQLITE_OK) {
-        fprintf(stderr, "SQL error: %s\n", zErrMsg);
-        sqlite3_free(zErrMsg);
-        return 1;
-    }
+       rc = sqlite3_exec(db, createTableQry.c_str(), NULL, 0, &zErrMsg);
+       if(rc != SQLITE_OK) {
+       fprintf(stderr, "SQL error: %s\n", zErrMsg);
+       sqlite3_free(zErrMsg);
+       return 1;
+       }
 
-    rc = sqlite3_exec(db, "BEGIN TRANSACTION;", NULL, 0, &zErrMsg);
-    if(rc != SQLITE_OK) {
-        fprintf(stderr, "SQL error: %s\n", zErrMsg);
-        sqlite3_free(zErrMsg);
-        return 1;
-    }
+       rc = sqlite3_exec(db, "PRAGMA synchronous=OFF;", NULL, 0, &zErrMsg);
+       if(rc != SQLITE_OK) {
+       fprintf(stderr, "SQL error: %s\n", zErrMsg);
+       sqlite3_free(zErrMsg);
+       return 1;
+       }
 
-    sqlite3_stmt *sqlStmt;
-    rc = sqlite3_prepare_v2(db, insertBindQry.c_str(), insertBindQry.length(), &sqlStmt, NULL);
-    if(rc != SQLITE_OK) {
-        fprintf(stderr, "SQL error %d: preparing failed\n", rc);
-        return 1;
-    }
+       rc = sqlite3_exec(db, "BEGIN TRANSACTION;", NULL, 0, &zErrMsg);
+       if(rc != SQLITE_OK) {
+       fprintf(stderr, "SQL error: %s\n", zErrMsg);
+       sqlite3_free(zErrMsg);
+       return 1;
+       }
 
+       sqlite3_stmt *sqlStmt;
+       rc = sqlite3_prepare_v2(db, insertBindQry.c_str(), insertBindQry.length(), &sqlStmt, NULL);
+       if(rc != SQLITE_OK) {
+       fprintf(stderr, "SQL error %d: preparing failed\n", rc);
+       return 1;
+       }
+       */
 
     string line;
 
     long recordCounter = 0;
     while(getline(inFile, line)) {
+        /*
         // if(isDebug) cout<<"line="<<line<<endl;
         // string insertQry;
         // insertQry = "INSERT INTO " + tableName + " VALUES (";
-
+        */
         int index = 0;
         int fieldCounter = 0;
         for (int i=0; i<fieldListCount; i++) {
@@ -220,20 +223,24 @@ int main(int argc, char **argv) {
                 value = trim(value);
                 // value = replaceAll(value, "'", "''");
                 if(value.length() > 0) {
-                    rc = sqlite3_bind_text(sqlStmt, fieldCounter + 1, value.c_str(), value.length(), SQLITE_TRANSIENT);
-                    if(rc != SQLITE_OK) {
-                        fprintf(stderr, "SQL error %d: text binding failed\n", rc);
-                        return 1;
-                    }
+                    /*
+                       rc = sqlite3_bind_text(sqlStmt, fieldCounter + 1, value.c_str(), value.length(), SQLITE_TRANSIENT);
+                       if(rc != SQLITE_OK) {
+                       fprintf(stderr, "SQL error %d: text binding failed\n", rc);
+                       return 1;
+                       }
                     // insertQry += "'" + value + "'";
+                    */
                 }
                 else {
-                    rc = sqlite3_bind_null(sqlStmt, fieldCounter + 1);
-                    if(rc != SQLITE_OK) {
-                        fprintf(stderr, "SQL error %d: text null binding failed\n", rc);
-                        return 1;
-                    }
+                    /*
+                       rc = sqlite3_bind_null(sqlStmt, fieldCounter + 1);
+                       if(rc != SQLITE_OK) {
+                       fprintf(stderr, "SQL error %d: text null binding failed\n", rc);
+                       return 1;
+                       }
                     // insertQry += " NULL";
+                    */
                 }
             }
 
@@ -242,20 +249,24 @@ int main(int argc, char **argv) {
                 value = trim(value);
                 value = trim(value, "0");
                 if(value.length() > 0) {
-                    rc = sqlite3_bind_int64(sqlStmt, fieldCounter + 1, atol(value.c_str()));
-                    if(rc != SQLITE_OK) {
-                        fprintf(stderr, "SQL error %d: int binding failed value=%s\n", rc, value.c_str());
-                        return 1;
-                    }
+                    /*
+                       rc = sqlite3_bind_int64(sqlStmt, fieldCounter + 1, atol(value.c_str()));
+                       if(rc != SQLITE_OK) {
+                       fprintf(stderr, "SQL error %d: int binding failed value=%s\n", rc, value.c_str());
+                       return 1;
+                       }
                     // insertQry += value;
+                    */
                 }
                 else {
-                    rc = sqlite3_bind_null(sqlStmt, fieldCounter + 1);
-                    if(rc != SQLITE_OK) {
-                        fprintf(stderr, "SQL error %d: int null binding failed\n", rc);
-                        return 1;
-                    }
+                    /*
+                       rc = sqlite3_bind_null(sqlStmt, fieldCounter + 1);
+                       if(rc != SQLITE_OK) {
+                       fprintf(stderr, "SQL error %d: int null binding failed\n", rc);
+                       return 1;
+                       }
                     // insertQry += " NULL";
+                    */
                 }
             }
 
@@ -264,20 +275,24 @@ int main(int argc, char **argv) {
                 value = trim(value);
                 value = trim(value, "0");
                 if(value.length() > 0) {
-                    rc = sqlite3_bind_double(sqlStmt, fieldCounter + 1, atof(value.c_str()));
-                    if(rc != SQLITE_OK) {
-                        fprintf(stderr, "SQL error %d: double binding failed value=%s\n", rc, value.c_str());
-                        return 1;
-                    }
+                    /*
+                       rc = sqlite3_bind_double(sqlStmt, fieldCounter + 1, atof(value.c_str()));
+                       if(rc != SQLITE_OK) {
+                       fprintf(stderr, "SQL error %d: double binding failed value=%s\n", rc, value.c_str());
+                       return 1;
+                       }
                     // insertQry += value;
+                    */
                 }
                 else {
-                    rc = sqlite3_bind_null(sqlStmt, fieldCounter + 1);
-                    if(rc != SQLITE_OK) {
-                        fprintf(stderr, "SQL error %d: double null binding failed\n", rc);
-                        return 1;
-                    }
+                    /*
+                       rc = sqlite3_bind_null(sqlStmt, fieldCounter + 1);
+                       if(rc != SQLITE_OK) {
+                       fprintf(stderr, "SQL error %d: double null binding failed\n", rc);
+                       return 1;
+                       }
                     // insertQry += " NULL";
+                    */
                 }
             }
             // if(isDebug) cout<<"valueAfter="<<value<<endl;
@@ -292,57 +307,62 @@ int main(int argc, char **argv) {
         // insertQry += ");";
         /*
 
-        rc = sqlite3_exec(db, insertQry.c_str(), NULL, 0, &zErrMsg);
-        if(rc != SQLITE_OK) {
-            fprintf(stderr, "SQL error: %s\n", zErrMsg);
-            sqlite3_free(zErrMsg);
-        }
-        */
-
-        rc = sqlite3_step(sqlStmt);
-        if(rc == SQLITE_DONE) {
-            rc = sqlite3_reset(sqlStmt);
-            if(rc != SQLITE_OK) {
-                fprintf(stderr, "SQL error %d: reset failed\n", rc);
-                return 1;
-            }
-        } else {
-            fprintf(stderr, "SQL error %d: step failed\n", rc);
-            return 1;
-        }
-
+           rc = sqlite3_exec(db, insertQry.c_str(), NULL, 0, &zErrMsg);
+           if(rc != SQLITE_OK) {
+           fprintf(stderr, "SQL error: %s\n", zErrMsg);
+           sqlite3_free(zErrMsg);
+           }
+           */
+        /*
+           rc = sqlite3_step(sqlStmt);
+           if(rc == SQLITE_DONE) {
+           rc = sqlite3_reset(sqlStmt);
+           if(rc != SQLITE_OK) {
+           fprintf(stderr, "SQL error %d: reset failed\n", rc);
+           return 1;
+           }
+           } else {
+           fprintf(stderr, "SQL error %d: step failed\n", rc);
+           return 1;
+           }
+           */
         // if(isDebug) cout<<"insertQry="<<insertQry<<endl;
         recordCounter++;
         if(recordCounter % commitAfter == 0) {
             // if(isDebug) cout<<"Committing at recordCounter="<<recordCounter<<endl;
-            rc = sqlite3_exec(db, "COMMIT;", NULL, 0, &zErrMsg);
-            if(rc != SQLITE_OK) {
-                fprintf(stderr, "SQL error: %s\n", zErrMsg);
-                sqlite3_free(zErrMsg);
-            }
+            /*
+               rc = sqlite3_exec(db, "COMMIT;", NULL, 0, &zErrMsg);
+               if(rc != SQLITE_OK) {
+               fprintf(stderr, "SQL error: %s\n", zErrMsg);
+               sqlite3_free(zErrMsg);
+               }
 
-            rc = sqlite3_exec(db, "BEGIN TRANSACTION;", NULL, 0, &zErrMsg);
-            if(rc != SQLITE_OK) {
-                fprintf(stderr, "SQL error: %s\n", zErrMsg);
-                sqlite3_free(zErrMsg);
-            }
+               rc = sqlite3_exec(db, "BEGIN TRANSACTION;", NULL, 0, &zErrMsg);
+               if(rc != SQLITE_OK) {
+               fprintf(stderr, "SQL error: %s\n", zErrMsg);
+               sqlite3_free(zErrMsg);
+               }
 
-            // sqlconn.commit();
+            */
         }
     }
 
-    rc = sqlite3_finalize(sqlStmt);
-    if(rc != SQLITE_OK) {
-        fprintf(stderr, "SQL error %d: reset failed\n", rc);
-    }
+    /*
+           rc = sqlite3_finalize(sqlStmt);
+           if(rc != SQLITE_OK) {
+           fprintf(stderr, "SQL error %d: reset failed\n", rc);
+           }
 
-    rc = sqlite3_exec(db, "COMMIT;", NULL, 0, &zErrMsg);
-    if(rc != SQLITE_OK) {
-        fprintf(stderr, "SQL error: %s\n", zErrMsg);
-        sqlite3_free(zErrMsg);
-    }
+           rc = sqlite3_exec(db, "COMMIT;", NULL, 0, &zErrMsg);
+           if(rc != SQLITE_OK) {
+           fprintf(stderr, "SQL error: %s\n", zErrMsg);
+           sqlite3_free(zErrMsg);
+           }
 
-    sqlite3_close(db);
+           sqlite3_close(db);
+    */
+    double timeInSecs = (clock() - cStartClock) / (double)CLOCKS_PER_SEC;
+    printf("Imported %ld records in %4.2f seconds with %.2f opts/sec \n", recordCounter, timeInSecs, recordCounter/timeInSecs);
     inFile.close();
     cJSON_Delete(root);
 }
