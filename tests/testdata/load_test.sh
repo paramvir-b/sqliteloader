300c7f824167d25f24cd5c70e6e7c5384563e935
paramvir-b <paramvir@rokoder.com>
2015-02-20 19:35:14 -0600

    Load test now is more dynamic

--- tests/testdata/load_test.sh
+++ tests/testdata/load_test.sh
@@ -38,6 +38,8 @@ run_sqlite_loader() {
     typeset layoutFile=$2
     typeset inputFile=$3
     typeset expOutputFile=$4
+    typeset readBufferSize=$5
+    typeset pragmaValues="$6"
     typeset inputOnlyFileName=${inputFile##*/}
     typeset outputFile=$workDir/${inputOnlyFileName%%.*}.db
     typeset actOutputFile=$workDir/${inputOnlyFileName%%.*}.act_csv
@@ -59,8 +61,13 @@ run_sqlite_loader() {
     echo outputFile=$outputFile
 
     rm -f $outputFile;
-    echo time ./sqliteloader -v -l $layoutFile -i $inputFile -t t -o $outputFile
-    time ./sqliteloader -v -s -l $layoutFile -i $inputFile -t t -o $outputFile
+    if [[ $pragmaValues == "" ]]; then
+        echo "time ./sqliteloader -v -l $layoutFile -i $inputFile -t t -o $outputFile ${readBufferSize:+-b $readBufferSize}"
+        time ./sqliteloader -v -s -l $layoutFile -i $inputFile -t t -o $outputFile ${readBufferSize:+-b $readBufferSize}
+    else
+        echo "time ./sqliteloader -v -l $layoutFile -i $inputFile -t t -o $outputFile ${readBufferSize:+-b $readBufferSize} -p $pragmaValues"
+        time ./sqliteloader -v -s -l $layoutFile -i $inputFile -t t -o $outputFile ${readBufferSize:+-b $readBufferSize} -p "$pragmaValues"
+    fi
 
     if [[ -e $expOutputFile ]]; then
         echo "Comparing $outputFile against expected file : $expOutputFile"
@@ -85,16 +92,65 @@ run_for_type() {
     typeset loadDir=$2
     typeset type=$3
     typeset num=$4
+    typeset readBufferSize=$5
+    typeset pragmaValues="$6"
     typeset loadFile=$loadDir/in_load_${type}_${num}.csv
 
-    run_sqlite_loader $workDir in_load_${type}_template_layout.json in_load_${type}_template.csv in_load_${type}_template.exp_csv
+    run_sqlite_loader $workDir in_load_${type}_template_layout.json in_load_${type}_template.csv in_load_${type}_template.exp_csv "$readBufferSize" "$pragmaValues"
     time create_load_test_file $workDir in_load_${type}_template.csv $loadFile $num $forceCreate
-    time run_sqlite_loader $workDir in_load_${type}_template_layout.json $loadFile
+    time run_sqlite_loader $workDir in_load_${type}_template_layout.json $loadFile "" "$readBufferSize" "$pragmaValues"
+}
+
+usage() {
+    echo "Usage: $0 [-f] [-c <N>] [-b <N>] [-p <comma-separated-pragmas>]"
+    echo "-f      Force create input csv files"
+    echo "-c      Number of times sample input files be repeated"
+    echo "-b      Read buffer size to be passed to sqliteloader. Refer to -b of sqliteloader"
+    echo "-p      Pragma values passed to sqliteloader. Refer to -p of sqliteloader"
 }
 
 WORK_DIR=./out
 LOAD_DIR=./load
-forceCreate=${1:-false}
+
+while getopts "fc:b:p:h" opt;
+do
+    case $opt in
+        f) forceCreate="true"
+            ;;
+        c) NUM_COUNT=$OPTARG
+            ;;
+        b) READ_BUFFER_SIZE=$OPTARG
+            ;;
+        p) PRAGMA_VALUES="$OPTARG"
+            ;;
+        h) usage
+            exit 1
+            ;;
+        ?)
+            usage
+            exit 1
+            ;;
+    esac
+done
+shift $((OPTIND-1))  #This tells getopts to move on to the next argument.
+
+if [[ $NUM_COUNT != "" && $NUM_COUNT != [0-9]* ]]; then
+    echo "-c must contain only numbers"
+    exit 1
+fi
+
+if [[ $READ_BUFFER_SIZE != "" && $READ_BUFFER_SIZE != [0-9]* ]]; then
+    echo "-b must contain only numbers"
+    exit 1
+fi
+
+forceCreate=${forceCreate:-false}
+NUM_COUNT=${NUM_COUNT:-19}
+
+echo forceCreate=$forceCreate
+echo NUM_COUNT=$NUM_COUNT
+echo READ_BUFFER_SIZE=$READ_BUFFER_SIZE
+echo PRAGMA_VALUES=$PRAGMA_VALUES
 
 rm -rf $WORK_DIR
 mkdir -p $WORK_DIR
@@ -111,13 +167,12 @@ if [[ ! -e $LOAD_DIR ]]; then
     fi
 fi
 
-NUM_COUNT=${2:-19}
-run_sqlite_loader $WORK_DIR in1_big_full_layout.json in1_big.csv in1_big_exp.csv
-run_for_type $WORK_DIR $LOAD_DIR "text" $NUM_COUNT 
-run_for_type $WORK_DIR $LOAD_DIR "integer" $NUM_COUNT
-run_for_type $WORK_DIR $LOAD_DIR "real" $NUM_COUNT
-run_for_type $WORK_DIR $LOAD_DIR "date" $NUM_COUNT
-run_for_type $WORK_DIR $LOAD_DIR "date_pivot" $NUM_COUNT
+run_sqlite_loader $WORK_DIR in1_big_full_layout.json in1_big.csv in1_big_exp.csv "$READ_BUFFER_SIZE" "$PRAGMA_VALUES"
+run_for_type $WORK_DIR $LOAD_DIR "text" $NUM_COUNT "$READ_BUFFER_SIZE" "$PRAGMA_VALUES" 
+run_for_type $WORK_DIR $LOAD_DIR "integer" $NUM_COUNT "$READ_BUFFER_SIZE" "$PRAGMA_VALUES"
+run_for_type $WORK_DIR $LOAD_DIR "real" $NUM_COUNT "$READ_BUFFER_SIZE" "$PRAGMA_VALUES"
+run_for_type $WORK_DIR $LOAD_DIR "date" $NUM_COUNT "$READ_BUFFER_SIZE" "$PRAGMA_VALUES"
+run_for_type $WORK_DIR $LOAD_DIR "date_pivot" $NUM_COUNT "$READ_BUFFER_SIZE" "$PRAGMA_VALUES"
 
 
 #time create_load_test_file in_load_template.csv in_load.csv 10 $forceCreate
